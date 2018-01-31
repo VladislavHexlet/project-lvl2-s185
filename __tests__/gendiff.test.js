d92aa9e09d51571819c6e749df23e8ce149e2a09
VladislavHexlet <vladislavprh1@gmail.com>
2018-01-31 18:05:38 +0300

    final step

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -13,25 +13,33 @@ const pathNewIni = '__tests__/__fixtures__/after.ini';
 const pathOldplain = '__tests__/__fixtures__/plain-before.json';
 const pathNewplain = '__tests__/__fixtures__/plain-after.json';
 
+const pathOldJsonOutput = '__tests__/__fixtures__/before-output-json.json';
+const pathNewJsonOutput = '__tests__/__fixtures__/after-output-json.json';
+
 const pathToResult = '__tests__/__fixtures__/result.txt';
 const pathToResultPlain = '__tests__/__fixtures__/plain-result.txt';
 
-it('test json format', () => {
+it('test json files', () => {
   const resultJson = fs.readFileSync(pathToResult, 'utf-8');
   expect(genDiff(pathOldJson, pathNewJson)).toBe(resultJson);
 });
 
-it('test yaml format', () => {
+it('test yaml files', () => {
   const resultYaml = fs.readFileSync(pathToResult, 'utf-8');
   expect(genDiff(pathOldYaml, pathNewYaml)).toBe(resultYaml);
 });
 
-it('test ini format', () => {
+it('test ini files', () => {
   const resultIni = fs.readFileSync(pathToResult, 'utf-8');
   expect(genDiff(pathOldIni, pathNewIni)).toBe(resultIni);
 });
 
 it('test plain format', () => {
-  const resultPlain = fs.readFileSync(pathToResultPlain, 'utf-8');
-  expect(genDiff(pathOldplain, pathNewplain, 'plain')).toBe(resultPlain);
+  const resultPlainFormat = fs.readFileSync(pathToResultPlain, 'utf-8');
+  expect(genDiff(pathOldplain, pathNewplain, 'plain')).toBe(resultPlainFormat);
+});
+
+it('test output in json format', () => {
+  const resultOutputJsonFormat = '{"children":[{"name":"host","type":"stayed","children":[],"value":{"previousValue":"hexlet.io","newValue":"hexlet.io"}},{"name":"timeout","type":"updated","children":[],"value":{"previousValue":50,"newValue":20}},{"name":"proxy","type":"removed","children":[],"value":{"previousValue":"123.234.53.22","newValue":""}},{"name":"verbose","type":"added","children":[],"value":{"previousValue":"","newValue":true}}]}';
+  expect(genDiff(pathOldJsonOutput, pathNewJsonOutput, 'json')).toBe(resultOutputJsonFormat);
 });
--- src/renderers/index.js
+++ src/renderers/index.js
@@ -1,9 +1,11 @@
 import plainRender from './plain';
 import standartRender from './standart';
+import jsonRender from './json';
 
 const renders = {
   standart: standartRender,
   plain: plainRender,
+  json: jsonRender,
 };
 
 export default type => renders[type];
